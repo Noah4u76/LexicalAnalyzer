47fe41f604301961f2d2b728ddb590158ce61351
Noah Sanderson <Noah4u76@gmail.com>
2025-02-28 13:15:06 -0800

    Implementation of Real, Operator, and Seperators FSMs

--- lexical_analyzer/finite_state_machines.py
+++ lexical_analyzer/finite_state_machines.py
@@ -64,3 +64,67 @@ class FiniteStateMachines:
                 return None
         
         return Token(TOKEN_INTEGER, result)
+    
+    def real(self):
+        """
+        Regular Expression: [0-9]+\.[0-9]+
+        """
+        if not (self.lexer.current_char is not None and self.lexer.current_char.isdigit()):
+            return None
+        
+        result = ''
+        
+        # Checking to make sure the first character is a digit our 1st state
+        while self.lexer.current_char is not None and self.lexer.current_char.isdigit():
+            result += self.lexer.current_char
+            self.lexer.advance()
+        
+        # Checking for a decimal point our 2nd state
+        if self.lexer.current_char != '.':
+            return None
+        
+        result += self.lexer.current_char
+        self.lexer.advance()
+        
+        # Checking to make sure there is digits after decimal point our 3rd state
+        if not (self.lexer.current_char is not None and self.lexer.current_char.isdigit()):
+            raise SyntaxError(f"Invalid real number format at line {self.lexer.line}, column {self.lexer.column}")
+        
+        while self.lexer.current_char is not None and self.lexer.current_char.isdigit():
+            result += self.lexer.current_char
+            self.lexer.advance()
+        
+        return Token(TOKEN_REAL, result)
+    
+    def operator(self):
+        """
+        Simple Finite State Machine for Operators
+        """
+        if not (self.lexer.current_char is not None and self.lexer.current_char in OPERATORS):
+            return None
+        
+        result = self.lexer.current_char
+        self.lexer.advance()
+        
+        if result in ["=", "<", ">","!"] and self.lexer.current_char == "=":
+            result += self.lexer.current_char
+            self.lexer.advance()
+        
+        if result in OPERATORS:
+            return Token(TOKEN_OPERATOR, result)
+        else:
+            raise SyntaxError(f"Invalid operator '{result}' at line {self.lexer.line}, column {self.lexer.column}")
+            
+        return Token(TOKEN_OPERATOR, result)
+    
+    def seperator(self):
+        """
+        Simple Finite State Machine for Separators
+        """
+        if not (self.lexer.current_char is not None and self.lexer.current_char in "(){}[],;:"):
+            return None
+        
+        result = self.lexer.current_char
+        self.lexer.advance()
+            
+        return Token(TOKEN_SEPARATOR, result)
