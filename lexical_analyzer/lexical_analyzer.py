aacb82af3dac51c5dc0601e36ffb534555958bcc
nnayrps19 <133690682+nnayrps19@users.noreply.github.com>
2025-03-02 08:30:04 -0800

    Added whitespace, and token functions to help deal with whitespaces in the code. Also created the token functions for the fsm to help identify and return the next tokens.

--- lexical_analyzer/lexical_analyzer.py
+++ lexical_analyzer/lexical_analyzer.py
@@ -1,4 +1,5 @@
 from token import Token
+from finite_state_machines import FiniteStateMachines
 from constants import TOKEN_KEYWORD, TOKEN_IDENTIFIER, TOKEN_INTEGER, TOKEN_REAL, TOKEN_OPERATOR, TOKEN_SEPARATOR
 from constants import KEYWORDS, OPERATORS, SEPARATORS
 
@@ -18,4 +19,27 @@ class Lexer:
             self.pos += 1
             self.column += 1
             self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
-            
+
+    def ship_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
+
+    def get_next_token(self):
+        self.skip_whitespace()
+        if self.current_char is None:
+            return None
+        
+        #Checking order of the FSM's
+        fsm = FiniteStateMachines(self)
+        for method in [fsm.identifier, fsm.integer, fsm.real, fsm.operator]:
+             token = method()
+             if token:
+                 return token
+        raise SyntaxError(f"Invalid Character '{self.current_char} at line {self.line}, column {self.column}")
+    
+    def tokenize(self):
+        tokens = []
+        while (token := self.get_next_token()) is not None:
+            tokens.append(token)
+        return tokens
